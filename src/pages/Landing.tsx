f81bc864ebc786531e11829b28b027b89fe49c83
0xardy <ardy@wonderland.xyz>
2026-02-11 13:42:49 -0300

    refactor: reuse shared footer component on landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,7 +1,7 @@
 import { Box, Button, styled, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { Footer } from "~/components/Footer";
 import { canonHeaderTokens } from "~/config/themes/safeTheme";
-import EthereumLogo from "~/assets/ethereum-logo.svg";
 
 export const Landing = () => {
   const navigate = useNavigate();
@@ -28,24 +28,7 @@ export const Landing = () => {
         </ButtonGroup>
       </HeroSection>
 
-      <Footer>
-        <FooterLeft>
-          <FooterLink>v1.0.0beta+fd0685</FooterLink>
-          <FooterLinkActive>Documentation</FooterLinkActive>
-          <FooterLinkActive>Security Audit</FooterLinkActive>
-          <FooterLinkActive>GitHub</FooterLinkActive>
-        </FooterLeft>
-
-        <FooterRight>
-          <PublicGoodSection>
-            <PublicGoodText>A public good for</PublicGoodText>
-            <EthLogoContainer>
-              <img src={EthereumLogo} alt='Ethereum' />
-            </EthLogoContainer>
-            <EthereumText>Ethereum</EthereumText>
-          </PublicGoodSection>
-        </FooterRight>
-      </Footer>
+      <Footer />
     </PageContainer>
   );
 };
@@ -216,116 +199,5 @@ const DocsButton = styled(Button)(({ theme }) => ({
   },
   [theme.breakpoints.down("md")]: {
     width: "100%",
-  },
-}));
-
-const Footer = styled(Box)(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "space-between",
-  width: "100%",
-  paddingTop: "64px",
-  paddingBottom: "24px",
-  paddingLeft: "24px",
-  paddingRight: "16px",
-  flexShrink: 0,
-  [theme.breakpoints.down("md")]: {
-    flexDirection: "column",
-    gap: "24px",
-    paddingTop: "32px",
-    alignItems: "flex-start",
-  },
-  [theme.breakpoints.down("sm")]: {
-    paddingLeft: "16px",
-    paddingRight: "16px",
-    gap: "16px",
-  },
-}));
-
-const FooterLeft = styled(Box)(({ theme }) => ({
-  display: "flex",
-  gap: "16px",
-  alignItems: "center",
-  fontSize: "13px",
-  fontWeight: 400,
-  flexShrink: 0,
-  flexWrap: "wrap",
-  [theme.breakpoints.down("sm")]: {
-    gap: "12px",
-    fontSize: "12px",
-  },
-}));
-
-const FooterLink = styled(Typography)(({ theme }) => ({
-  fontSize: "13px",
-  lineHeight: "16px",
-  color: canonHeaderTokens.foreground.accent30,
-  flexShrink: 0,
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "12px",
-  },
-}));
-
-const FooterLinkActive = styled(Typography)(({ theme }) => ({
-  fontSize: "13px",
-  lineHeight: "16px",
-  color: canonHeaderTokens.foreground.accent20,
-  flexShrink: 0,
-  cursor: "pointer",
-  "&:hover": {
-    color: canonHeaderTokens.foreground.accent10,
-  },
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "12px",
-  },
-}));
-
-const FooterRight = styled(Box)({
-  display: "flex",
-  gap: "16px",
-  alignItems: "center",
-  flexShrink: 0,
-});
-
-const PublicGoodSection = styled(Box)({
-  display: "flex",
-  gap: "8px",
-  alignItems: "center",
-  flexShrink: 0,
-});
-
-const PublicGoodText = styled(Typography)(({ theme }) => ({
-  fontSize: "13px",
-  fontStyle: "italic",
-  fontWeight: 400,
-  lineHeight: "16px",
-  color: canonHeaderTokens.foreground.accent30,
-  flexShrink: 0,
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "12px",
-  },
-}));
-
-const EthLogoContainer = styled(Box)({
-  height: "14px",
-  width: "8.595px",
-  flexShrink: 0,
-  overflow: "clip",
-  "& img": {
-    display: "block",
-    maxWidth: "none",
-    width: "100%",
-    height: "100%",
-  },
-});
-
-const EthereumText = styled(Typography)(({ theme }) => ({
-  fontSize: "13px",
-  fontWeight: 600,
-  lineHeight: "16px",
-  color: canonHeaderTokens.foreground.accent20,
-  flexShrink: 0,
-  [theme.breakpoints.down("sm")]: {
-    fontSize: "12px",
   },
 }));
